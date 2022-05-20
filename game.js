0349cecbe49579055f22ad9ba86a3ab0a715d96f
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-20 23:21:31 +0300

    Added score UI

--- game.js
+++ game.js
@@ -3,33 +3,27 @@ var score = 0;
 
 function press_start() {
     document.getElementById("start").addEventListener("click", reset_game, false);
-    document
-        .getElementsByClassName("boundary")
-        .item(4)
-        .innerHTML("SCORE:", score);
 }
 
 function start_game() {
+    score = 0;
     for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
         document
             .getElementsByClassName("boundary")
             .item(i)
             .addEventListener("mouseover", color_divs);
     }
-    document.getElementById("end").addEventListener("click", end_game, false);
+    document.getElementById("end").addEventListener("click", end_game, true);
 
-    //score -10 when lose, +5 when win
-    //win lose b id status
-    //change innerhtml to you lost
     //handle cheating alert,onmouseleave  id game
 }
 
 function color_divs() {
+    score -= 10;
     var boundaries = document.getElementsByClassName("boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries.item(i).style.backgroundColor = "red";
     }
-    score -= 10;
 }
 
 function reset_game() {
@@ -42,13 +36,18 @@ function reset_game() {
 }
 
 function end_game() {
+    score += 5;
     for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
         document
             .getElementsByClassName("boundary")
             .item(i)
-            .removeEventListener("mouseover", color_divs);
+            .removeEventListener("mouseover", color_divs, true);
     }
-    document.getElementById("end").removeEventListener("click", end_game, false);
-    if (score > 0) document.getElementById("status").innerHTML("You Win");
-    else document.getElementById("status").innerHTML("You Lose");
+    //document.getElementById("end").removeEventListener("click", end_game, false);
+    if (score > 0) document.getElementById("status").innerHTML = "You Win";
+    else document.getElementById("status").innerHTML = "You Lose";
+    document.getElementsByClassName("example").item(0).innerHTML =
+        "SCORE:" + score;
+    console.log(score);
+    reset_game();
 }
