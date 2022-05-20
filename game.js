e8d34a66ad9525f73f00ac934bf9857b52cc719d
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-20 22:45:27 +0300

    Added onclick for start button

--- game.js
+++ game.js
@@ -1,23 +1,40 @@
-window.addEventListener("load", start_game, false);
+window.addEventListener("load", press_start, false);
+var score = 0;
+
+function press_start() {
+    document.getElementById("start").addEventListener("click", reset_game, false);
+}
 
 function start_game() {
-    //start on the s, once we click on it game starts w everything goes back to normal
     for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
         document
             .getElementsByClassName("boundary")
             .item(i)
-            .addEventListener("mouseover", colorDivs);
+            .addEventListener("mouseover", color_divs);
     }
     //score -10 when lose, +5 when win
     //win lose b id status
     //change innerhtml to you lost
     //handle cheating alert
     //after clicking on E, the game should end all conditions should no longer be valid
+    //onmouseleave  id game
 }
 
-function colorDivs() {
+function color_divs() {
     var boundaries = document.getElementsByClassName("boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries.item(i).style.backgroundColor = "red";
     }
+    score -= 10;
 }
+
+function reset_game() {
+    var boundaries = document.getElementsByClassName("boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries.item(i).style.backgroundColor = "#eeeeee";
+    }
+    score = 0;
+    start_game();
+}
+
+function end_game() {}
