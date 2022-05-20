660c8ea5600fcb3d57521bdeeb8cb7531c753b12
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-21 00:50:47 +0300

    Fixed all conditions: eventlisteners weren't removed well before, and score wasn't updating well

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", press_start, false);
 var score = 0;
+var boundaries;
+var outside_game;
+var e_box;
+var scoretext;
 
 function press_start() {
     //when clicking S, reset the game
@@ -8,20 +12,18 @@ function press_start() {
 
 function start_game() {
     //detect the mouse moves, if it hit one of the walls, change the walls's colors to red
-    for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
-        document
-            .getElementsByClassName("boundary")
-            .item(i)
-            .addEventListener("mouseover", color_divs, true);
+    boundaries = document.getElementsByClassName("boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries.item(i).addEventListener("mouseover", color_divs, true);
     }
 
     //if the user after starting the game tried to go to the end outside the game, he's considered cheating
-    document
-        .getElementById("game")
-        .addEventListener("mouseleave", cheating_alert, false);
+    outside_game = document.getElementById("game");
+    outside_game.addEventListener("mouseleave", cheating_alert, false);
 
     //if the user clicks on E, the game ends
-    document.getElementById("end").addEventListener("click", end_game, true);
+    e_box = document.getElementById("end");
+    e_box.addEventListener("click", end_game, true);
 }
 
 function color_divs() {
@@ -29,22 +31,24 @@ function color_divs() {
     score -= 10;
 
     //update the score
-    document.getElementsByClassName("example").item(0).innerHTML =
-        "SCORE:" + score;
+    //scoretext = document.getElementsByClassName("example").item(0);
+    scoretext.innerHTML = "SCORE:" + score;
 
     //change color of walls to red
-    var boundaries = document.getElementsByClassName("boundary");
+    //boundaries = document.getElementsByClassName("boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries.item(i).style.backgroundColor = "red";
     }
 }
 
 function reset_game() {
-    //return the score back to 0
+    //initial score before game starts or after reset back to 0
     score = 0;
+    scoretext = document.getElementsByClassName("example").item(0);
+    scoretext.innerHTML = "SCORE:" + score;
 
     //return the original colors of the walls
-    var boundaries = document.getElementsByClassName("boundary");
+    boundaries = document.getElementsByClassName("boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries.item(i).style.backgroundColor = "#eeeeee";
     }
@@ -57,25 +61,38 @@ function end_game() {
 
     //check if the score is positive, and tell the user that he won, otherwise he lost
     if (score > 0) document.getElementById("status").innerHTML = "You Win";
-    else document.getElementById("status").innerHTML = "You Lose";
+    else if (score < 0) document.getElementById("status").innerHTML = "You Lose";
+    else
+        document.getElementById("status").innerHTML =
+        'Begin by moving your mouse over the "S".';
 
     //update the final score
-    document.getElementsByClassName("example").item(0).innerHTML =
-        "SCORE:" + score;
+    scoretext.innerHTML = "SCORE:" + score;
 
     //remove eventlisteners when game ends, to stop handling userclicks
-    for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
-        document
-            .getElementsByClassName("boundary")
-            .item(i)
-            .removeEventListener("mouseover", color_divs, true);
-    }
-    document
-        .getElementById("game")
-        .removeEventListener("mouseleave", cheating_alert, false);
-    reset_game();
+    remover();
+
+    //update the final score
+    scoretext.innerHTML = "SCORE:" + score;
 }
 
 function cheating_alert() {
+    //alert the user when he's caught cheating
     window.alert("Caught you cheating!");
+
+    //remove all eventlisteners because he cheated, he should restart the game to play
+    remover();
+}
+
+//called to remove eventlisteners
+function remover() {
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries.item(i).removeEventListener("mouseover", color_divs, true);
+    }
+    outside_game.removeEventListener("mouseleave", cheating_alert, false);
+    boundaries = document.getElementsByClassName("boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries.item(i).style.backgroundColor = "#eeeeee";
+    }
+    e_box.removeEventListener("click", end_game, true);
 }
