7117b9716a265f48735009e5762c95875c4bda66
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-21 00:17:28 +0300

    Added comments to describe each section

--- game.js
+++ game.js
@@ -2,26 +2,37 @@ window.addEventListener("load", press_start, false);
 var score = 0;
 
 function press_start() {
+    //when clicking S, reset the game
     document.getElementById("start").addEventListener("click", reset_game, true);
 }
 
 function start_game() {
+    //detect the mouse moves, if it hit one of the walls, change the walls's colors to red
     for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
         document
             .getElementsByClassName("boundary")
             .item(i)
             .addEventListener("mouseover", color_divs, true);
     }
-    document.getElementById("end").addEventListener("click", end_game, true);
+
+    //if the user after starting the game tried to go to the end outside the game, he's considered cheating
     document
         .getElementById("game")
         .addEventListener("mouseleave", cheating_alert, false);
+
+    //if the user clicks on E, the game ends
+    document.getElementById("end").addEventListener("click", end_game, true);
 }
 
 function color_divs() {
+    //decrement the score by 10 upon each wall hit
     score -= 10;
+
+    //update the score
     document.getElementsByClassName("example").item(0).innerHTML =
         "SCORE:" + score;
+
+    //change color of walls to red
     var boundaries = document.getElementsByClassName("boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries.item(i).style.backgroundColor = "red";
@@ -29,8 +40,10 @@ function color_divs() {
 }
 
 function reset_game() {
+    //return the score back to 0
     score = 0;
-    document.getElementById("end").removeEventListener("click", end_game, true);
+
+    //return the original colors of the walls
     var boundaries = document.getElementsByClassName("boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries.item(i).style.backgroundColor = "#eeeeee";
@@ -39,17 +52,27 @@ function reset_game() {
 }
 
 function end_game() {
+    //increment the score if the user reached the end
     score += 5;
+
+    //check if the score is positive, and tell the user that he won, otherwise he lost
+    if (score > 0) document.getElementById("status").innerHTML = "You Win";
+    else document.getElementById("status").innerHTML = "You Lose";
+
+    //update the final score
+    document.getElementsByClassName("example").item(0).innerHTML =
+        "SCORE:" + score;
+
+    //remove eventlisteners when game ends, to stop handling userclicks
     for (var i = 0; i < document.getElementsByClassName("boundary").length; i++) {
         document
             .getElementsByClassName("boundary")
             .item(i)
             .removeEventListener("mouseover", color_divs, true);
     }
-    if (score > 0) document.getElementById("status").innerHTML = "You Win";
-    else document.getElementById("status").innerHTML = "You Lose";
-    document.getElementsByClassName("example").item(0).innerHTML =
-        "SCORE:" + score;
+    document
+        .getElementById("game")
+        .removeEventListener("mouseleave", cheating_alert, false);
     reset_game();
 }
 
